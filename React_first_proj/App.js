3b1a3c75cf1adc53305276b67b24bc61889e2c87
mudits02 <mudits02@gmail.com>
2024-01-22 00:32:52 +0530

    babel basics

--- React_first_proj/App.js
+++ React_first_proj/App.js
@@ -1,26 +1,7 @@
-    //Creates a React object for you
-    //const heading = React.createElement("h1" , {id : "heading" , someRandomAttribute : "Loda"} , "Hello World from React");
-    /**
-     * <div id="parent">
-     *      <div id="child"
-     *          <h1> I am h1 ..... </h1>
-     *          <h2> I am h2 ... </h2>
-     *      </div>
-     * </div>
-     */
+import React from "react";
+import ReactDOM from "react-dom";
 
-    import React from "react";
-    import ReactDOM from "react-dom";
+const jsxHeading = <h1 id="heading">Heading using JSX</h1>
 
-    const parent = React.createElement("div" , {id : "parent"} , 
-    React.createElement("div" , {id : "child"} , 
-    React.createElement("h1" , {} , ["I am h1 from inside the nested divs" , 
-    React.createElement('h2' , {} , "h2 tag from nested div")
-    ])
-    ))
-    console.log(parent)
-    //This makes it for the browser to understand
-    const root = ReactDOM.createRoot(document.getElementById("root"));
-
-    //rendeer method takes this object converts this root object into actual heading and puts it on the browser
-    root.render(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(jsxHeading);
