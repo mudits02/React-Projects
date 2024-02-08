b76a489f4e51860c1ee1f46c226fcc841b5c58ad
mudits02 <mudits02@gmail.com>
2024-02-08 23:03:51 +0530

    JSX Intro

--- React_first_proj/App.js
+++ React_first_proj/App.js
@@ -1,14 +1,42 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+// React Element
 const jsxHeading = (
     <h1 className="head">
     Heading using JSX
     </h1>
 );
 
-console.log(jsxHeading);
+//console.log(jsxHeading);
+
+const Title = () =>
+(
+    <h1>
+        Injected functions Component from the title
+    </h1>
+);
+
+const number = 1000;
+
+// Functional Component
+const HeadingComponent = () =>
+(
+    
+    
+    <div id="container">
+        <Title />
+        {number}
+        <h2>{100 + 200}</h2>
+        <h1 className="container">Call from the functional Component</h1>
+        
+    </div>
+    
+);
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(jsxHeading);
+root.render(<HeadingComponent />);
