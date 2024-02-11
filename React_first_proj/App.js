63ebe5b5568f05bf3af3a4a4bb082ffbe15e5e71
mudits02 <mudits02@gmail.com>
2024-02-11 12:22:09 +0530

    Website -1

--- React_first_proj/App.js
+++ React_first_proj/App.js
@@ -1,43 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// React Element
-const jsxHeading = (
-    <h1 className="head">
-    Heading using JSX
-    </h1>
-);
 
-//console.log(jsxHeading);
-
-const Title = () =>
-(
-    <h1>
-        Injected functions Component from the title
-    </h1>
-);
-
-const number = 1000;
-
-// Functional Component
-const HeadingComponent = () =>
-(
-    
-    
-    <div id="container">
-        <Title />
-        {number}
-        {jsxHeading} {/* this is how we call React elements inside of a component */}
-        <h2>{100 + 200}</h2>
-        <h1 className="container">Call from the functional Component</h1>
-        
-    </div>
-    
-);
+/**
+ * Header 
+ *  - Logo
+ *  - Nav Items
+ * Body 
+ *  - Search
+ *  - Restaurant Container
+ *      -Restaurant Card
+ * Footer
+ *  - Copyright
+ *  - Links
+ */
 
 
-
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(<HeadingComponent />);
