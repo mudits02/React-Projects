b223ad31215a811af6f5b2a5905d6a7ef4f37ac3
mudits02 <mudits02@gmail.com>
2024-03-24 17:16:40 +0530

    Minor fixes

--- React_first_proj/prev_code.js
+++ React_first_proj/prev_code.js
@@ -1,44 +1,113 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+// import React from "react";
+// import ReactDOM from "react-dom/client";
 
-// React Element
-const jsxHeading = (
-    <h1 className="head">
-    Heading using JSX
-    </h1>
-);
+// // React Element
+// const jsxHeading = (
+//     <h1 className="head">
+//     Heading using JSX
+//     </h1>
+// );
 
-//console.log(jsxHeading);
+// //console.log(jsxHeading);
 
-const Title = () =>
-(
-    <h1>
-        Injected functions Component from the title
-    </h1>
-);
+// const Title = () =>
+// (
+//     <h1>
+//         Injected functions Component from the title
+//     </h1>
+// );
 
-const number = 1000;
+// const number = 1000;
 
-// Functional Component
-const HeadingComponent = () =>
-(
+// // Functional Component
+// const HeadingComponent = () =>
+// (
     
     
-    <div id="container">
-        <Title />
-        {number}
-        {jsxHeading} {/* this is how we call React elements inside of a component */}
-        <h2>{100 + 200}</h2>
-        <h1 className="container">Call from the functional Component</h1>
-        {Title()} {/* This is the another way to call a componenet inside of a component*/ }
+//     <div id="container">
+//         <Title />
+//         {number}
+//         {jsxHeading} {/* this is how we call React elements inside of a component */}
+//         <h2>{100 + 200}</h2>
+//         <h1 className="container">Call from the functional Component</h1>
+//         {Title()} {/* This is the another way to call a componenet inside of a component*/ }
         
-    </div>
+//     </div>
     
-);
+// );
 
 
 
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+// const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeadingComponent />);
+// root.render(<HeadingComponent />);
+
+// console.log(5 + "9");
+
+// console.log(typeof NaN);
+// console.log(typeof 2);
+// console.log(typeof "2");
+
+// console.log(0.1 + 0.2 === 0.3);
+// console.log([] == []);
+// console.log([] === []);
+// var x = 5;
+// var y = "5";
+// console.log(x == y);
+// console.log(+"5" + 3);
+// var x = "5" * 3;
+// console.log(typeof x);
+// console.log("5" / 2);
+// var x = 5;
+// console.log(x++);
+// var x = 10;
+// function foo() {
+//     console.log(x);
+//     var m = 20;
+    
+    
+// }
+// foo();
+
+
+// console.log(mudit);
+// var mudit = 14;
+
+// console.log(foo());
+// function foo() {
+//     return "Hello, world!";
+// }
+// console.log(foo());
+// var x = 5;
+// function foo() {
+//     var x = 5;
+//     console.log(x);
+//     var x = 10;
+// }
+
+// console.log(greeting()); // "Hello there!"
+ 
+ 
+// function greeting() {
+//  return "Hello there!";
+// }
+
+// function c() {
+//     console.log(foo);
+//     console.log(bar);
+//     var foo = "Hello";
+//     let bar = "World";
+// }
+// c();
+
+
+
+
+
+
+
+
+
+
+
