cbaea92edb10113311e639d1c72d4381909bc663
mudits02 <mudits02@gmail.com>
2024-04-11 10:52:04 +0530

    Minor changes

--- React_first_proj/prev_code.js
+++ React_first_proj/prev_code.js
@@ -1,47 +1,47 @@
-// import React from "react";
-// import ReactDOM from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-// // React Element
-// const jsxHeading = (
-//     <h1 className="head">
-//     Heading using JSX
-//     </h1>
-// );
+// React Element
+const jsxHeading = (
+    <h1 className="head">
+    Heading using JSX
+    </h1>
+);
 
-// //console.log(jsxHeading);
+//console.log(jsxHeading);
 
-// const Title = () =>
-// (
-//     <h1>
-//         Injected functions Component from the title
-//     </h1>
-// );
+const Title = () =>
+(
+    <h1>
+        Injected functions Component from the title
+    </h1>
+);
 
-// const number = 1000;
+const number = 1000;
 
-// // Functional Component
-// const HeadingComponent = () =>
-// (
+// Functional Component
+const HeadingComponent = () =>
+(
     
     
-//     <div id="container">
-//         <Title />
-//         {number}
-//         {jsxHeading} {/* this is how we call React elements inside of a component */}
-//         <h2>{100 + 200}</h2>
-//         <h1 className="container">Call from the functional Component</h1>
-//         {Title()} {/* This is the another way to call a componenet inside of a component*/ }
+    <div id="container">
+        <Title />
+        {number}
+        {jsxHeading} {/* this is how we call React elements inside of a component */}
+        <h2>{100 + 200}</h2>
+        <h1 className="container">Call from the functional Component</h1>
+        {Title()} {/* This is the another way to call a componenet inside of a component*/ }
         
-//     </div>
+    </div>
     
-// );
+);
 
 
 
 
-// const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// root.render(<HeadingComponent />);
+root.render(<HeadingComponent />);
 
 // console.log(5 + "9");
 
