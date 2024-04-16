1fb19efcfe18e556f68e411d99fcd75a67ddead7
mudits02 <mudits02@gmail.com>
2024-04-17 01:52:04 +0530

    Hooks implemented in filter command

--- React_first_proj/src/componenets/Body.js
+++ React_first_proj/src/componenets/Body.js
@@ -2,9 +2,12 @@
 // We are mapping restaurantList array and passing data to RestaurantCard component as props with unique key as index
 import RestaurantCard from "./RestaurantCard";
 import restaurantList from "../utils/mockData";
+import {useState} from "react"; // Named import
+
 
   export const Body = () => {
 
+    const [listOfRestaurants , setListOFRestaurants] = useState(restaurantList);
 
     return (
       <div className="body">
@@ -12,7 +15,13 @@ import restaurantList from "../utils/mockData";
       <div className="search">Filter
         <button 
         className="filter-btn" onClick={() => {
-          alert("Filter button activated");
+          const filteredList = listOfRestaurants.filter((res) => 
+            res.data.avgRating > 4
+            //console.log(res);
+          );
+ 
+          setListOFRestaurants(filteredList);
+          
         }}>
           Top Rated
         </button>
@@ -20,7 +29,7 @@ import restaurantList from "../utils/mockData";
 
 
       <div className="res-container">
-          {restaurantList.map((restaurant) => {
+          {listOfRestaurants.map((restaurant) => {
             return <RestaurantCard key={restaurant.data.id} resData={restaurant} />;
           })}
         </div>
