88680620f3ff81149146337b554f3c1c75893fec
mudits02 <mudits02@gmail.com>
2024-04-25 21:56:13 +0530

    API integrated

--- React_first_proj/src/componenets/Body.js
+++ React_first_proj/src/componenets/Body.js
@@ -2,12 +2,25 @@
 // We are mapping restaurantList array and passing data to RestaurantCard component as props with unique key as index
 import RestaurantCard from "./RestaurantCard";
 import restaurantList from "../utils/mockData";
-import {useState} from "react"; // Named import
-
+import {useState , useEffect} from "react"; // Named import
 
   export const Body = () => {
 
     const [listOfRestaurants , setListOFRestaurants] = useState(restaurantList);
+
+    useEffect(() => {
+      fetchData();
+    } , []);
+
+    const fetchData = async () => {
+      const data = fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.45970&lng=77.02820&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      );
+
+      const json = await data.json();
+
+      console.log(json);
+      setListOFRestaurants(json.data.cards[4]);
+    };
 
     return (
       <div className="body">
