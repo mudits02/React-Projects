5eeeaf37b10ecca1f23415b2a659824826998eab
mudits02 <mudits02@gmail.com>
2024-05-11 12:31:49 +0530

    Class BAsed component added

--- React_first_proj/src/components/About.js
+++ React_first_proj/src/components/About.js
@@ -1,12 +1,34 @@
 import UserClass from "./UserClass"
+import React from "react";
 
-const About = () => {
-    return(
-        <div className="about-page">
-            <h1>About Us</h1>
-            <UserClass name={"Mudit Shukla (Class)"} location={"Gwalior (Class)"} twitter={"mudits02 (Class)"} />
-        </div>
-    )
+
+class About extends React.Component{
+
+
+    constructor(props)
+    {
+        super(props);
+        console.log("Parent Constructor");
+    }
+
+    componentDidMount()
+    {
+        console.log("Parent Did Mount");
+    }
+
+    render()
+    {
+        console.log("Parent Render");
+        return(
+            <div className="about-page">
+                <h1>About Us</h1>
+                <UserClass name={"First Child (Class)"} location={"Gwalior (Class)"} twitter={"mudits02 (Class)"} />
+            </div>
+        )
+    }
+   
 }
+    
+
 
 export default About;
--- React_first_proj/src/components/UserClass.js
+++ React_first_proj/src/components/UserClass.js
@@ -5,23 +5,46 @@ class UserClass extends React.Component{
     constructor(props)
     {
         super(props);
+        console.log(this.props.name + "Child Constructor")
+        //In class based components, we use just keep adding the state variables in the object
+        // Like below
         this.state = {
-            count : 0 ,
-            count2 : 1,
+            userInfo : {
+                name: "Dummy Name",
+                location: "Default Location",
+                company: "Jhaant",
+            }
         };
     };
 
-    render(){
+    async componentDidMount()
+    {
+        const data = await fetch("https://api.github.com/users/mudits02");
+        const json = await data.json();
 
-        const {name , location , twitter} = this.props;
-        const {count} = this.state;
+        this.setState({
+            userInfo: json,
+        })
+        console.log(json);
+    }
+
+    componentDidUpdate()
+    {
+        console.log("Component Did Update");
+    }
+
+    render(){
+        console.log(this.props.name + "Child Render")
+        const {name , location , avatar_url , company} = this.state.userInfo;
+        //const {count} = this.state;
 
         return(
             <div className="user-card">
-                <h1>Count : {count}</h1>
+                <img src={avatar_url} className="about_us_avatar"/>
                 <h2>Name : {name}</h2>
                 <h3>Location : {location}</h3>
-                <h3>Twitter: {twitter}</h3>
+                <h3>Company: {company}</h3>
+
             </div>
         )
     }
